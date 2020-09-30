c5579330c59c91a4cfa293ff6c59255816a2fcce
Mohamed Amine BEN fatma <benfatma1997@gmail.com>
2020-09-30 15:09:36 +0200

    modif4

--- acquisition1.m
+++ acquisition1.m
@@ -6,11 +6,15 @@ fe=16.368e6; % fréquence d'échantillonnage
 fi=2.046e6; % fréquence intermédiaire
 Tc=1e-3; % période du code C/A
 nbr_ech=fe*Tc; % nombre d'échantillons par période de code(cad sur une millisec)
-
+K=1;N=5;
 prn=10; % numéro PRN du satellite à étudier
 
 ca = 2*gcacode(prn)-1; % code C/A du satellite à étudier
 sca = sampleca(ca,fe); % code rééchantillonné à la fréquence fe
+scaNms=[];
+for i=1:N
+    scaNms=[scaNms sca];
+end;
 
 figure
 plot(sca)
@@ -23,7 +27,7 @@ fid = fopen('rec.bin.091','r','l'); % ouverture du fichier en lecture, format li
 
 data01=fread(fid,nbr_ech,'ubit1','l'); % lecture du signal sur 1 période de code
 data=2*data01-1; % conversion de binaire en -1 / 1
-Rsum = zeros(length(t),
+
 figure
 plot(data)
 axis([0 length(data) -1.2 1.2])
@@ -31,52 +35,38 @@ xlabel('Echantillon')
 ylabel('Signal d''antenne')
 title('Signal brut en sortie d''antenne, échantillonné à 16.368 MHz')
 %Acquisitionpar FFT
-R=[];
-ftest=-5000:100:5000;
-for deltaf=ftest  %for Q3
-    %deltaf = -2660; %frequence Dopler du satellite 10 Q2
-    fs=fi+deltaf;
-    t=0:1/fe:1e-3-1/fe;
-    porteusec=cos(2*pi*fs*t);
-    porteuse=sin(2*pi*fs*t);
-    I=data.*porteusec.' ; 
-    Q=data.*porteuse.' ;
-    X=fft(I+1i*Q);
-    Y= conj(fft(sca));
-    r2=(ifft(X.*Y.')).^2;
-    R=[R r2];
-    
-end;
 
+
+ftest=-5000:100:5000;
+ t=0:1/fe:N*1e-3-1/fe;
+Rsum=zeros(length(t), length(ftest));
 for k=1:K
-    
-    data01=fread(fid,nbr_ech,'ubit1','l'); % lecture du signal sur 1 période de code
+    data01=fread(fid,nbr_ech*N,'ubit1','l'); % lecture du signal sur 1 période de code
     data=2*data01-1; % conversion de binaire en -1 / 1
 
     R=[];
-    ftest=-5000:100:5000;
+
     for deltaf=ftest  %for Q3
         %deltaf = -2660; %frequence Dopler du satellite 10 Q2
         fs=fi+deltaf;
-        t=0:1/fe:1e-3-1/fe;
+       
         porteusec=cos(2*pi*fs*t);
         porteuse=sin(2*pi*fs*t);
         I=data.*porteusec.' ; 
         Q=data.*porteuse.' ;
         X=fft(I+1i*Q);
-        Y= conj(fft(sca));
+        Y= conj(fft(scaNms));
         r2=(ifft(X.*Y.')).^2;
         R=[R r2];
-        Rsum+=Rsum +R
-
+        
     end;
+    Rsum=Rsum+R;
 end;
 figure
 %plot(abs(r2))
 mesh(ftest,t,abs(R))
-mesh(ftest,t,abs(R))
-
-
+figure
+mesh(ftest,t,abs(Rsum))
 
 
 fclose(fid); % fermeture du fichier
